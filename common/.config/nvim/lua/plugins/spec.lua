4ccb513248a68411e443504392f35ddb5c46653c
melogale <zachariabadgers@gmail.com>
2026-01-02 23:55:21 -0500

    term color nvim

--- common/.config/nvim/lua/plugins/spec.lua
+++ common/.config/nvim/lua/plugins/spec.lua
@@ -1,12 +1,12 @@
 return {
   -- the colorscheme should be available when starting Neovim
   {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    "RRethy/nvim-base16",
+    lazy = false,
+    priority = 1000,
     config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
+      vim.o.termguicolors = false
+      vim.cmd.colorscheme("base16-default-dark")
     end,
   },
 
